8b55aff4156992c83c60f4380da233dca9b7f719
Camila Luísa Farias <luisafarias@MacBook-Pro-de-Camila.local>
2017-09-13 19:27:49 -0300

    mais avancos

--- MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/NewUser.java
+++ MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/NewUser.java
@@ -1,7 +1,9 @@
 package com.example.luisafarias.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewUser extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class NewUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+    }
+
+    public void goSemFeed(View view){
+        Intent intent = new Intent(this,SemFeed.class);
+        startActivity(intent);
     }
 }
