cb7ace8edb9ef7647d7b6b0b713834a2f00b6fd5
Camila Luísa Farias <luisafarias@MacBook-Pro-de-Camila.local>
2017-09-19 19:28:38 -0300

    menu feito e alguns erros tbm

--- MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/NewUrl.java
+++ MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/NewUrl.java
@@ -3,23 +3,65 @@ package com.example.luisafarias.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.wedeploy.android.Callback;
+import com.wedeploy.android.WeDeploy;
+import com.wedeploy.android.exception.WeDeployException;
+import com.wedeploy.android.transport.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class NewUrl extends AppCompatActivity {
 
+    WeDeploy weDeploy = new WeDeploy.Builder().build();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_url);
     }
 
-    public void addNewUrl(View view){
-        TextView nomeUrl = (TextView) findViewById(R.id.nomeUrl);
-        String nomeurl = nomeUrl.getText().toString();
+    public void addNewUrl(View view) throws JSONException {
+        TextView nomeUrl1 = (TextView) findViewById(R.id.nomeUrl);
+        String nomeUrl = nomeUrl1.getText().toString();
         TextView url1 = (TextView) findViewById(R.id.url);
         String url = url1.getText().toString();
+        String userId = "userId";
+        weDeploy //get userId nao esta funcionando, preciso saber qual o tipo da variavel para pegar o id
+                .auth("https://auth-weread.wedeploy.io")
+                .getCurrentUser()
+                .execute(new Callback() {
+                    public void onSuccess(Response response) {
+                        // here you receive the movies
+                    }
 
+                    public void onFailure(Exception e) {
+                        Log.e(NewUser.class.getName(),e.getMessage());
+                    }
+                });
+
+
+
+        JSONObject feedJsonObject = new JSONObject()
+                .put("name", nomeUrl)
+                .put("userId", userId)
+                .put("url", url);
+
+        weDeploy
+                .data("https://data-weread.wedeploy.io")
+               .create("Feeds", feedJsonObject)
+                        .execute(new Callback() {
+                            public void onSuccess(Response response) {
+                                // here you receive the movies
+                            }
+
+                            public void onFailure(Exception e) {
+                                Log.e(NewUser.class.getName(),e.getMessage());
+                            }
+                        });
 
     }
 }
--- MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/SemFeed.java
+++ MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/SemFeed.java
@@ -4,14 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class SemFeed extends AppCompatActivity {
+import com.wedeploy.android.Callback;
+import com.wedeploy.android.WeDeploy;
+import com.wedeploy.android.exception.WeDeployException;
+import com.wedeploy.android.transport.Response;
 
+public class SemFeed extends AppCompatActivity {
+    WeDeploy weDeploy = new WeDeploy.Builder().build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +41,20 @@ public class SemFeed extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         int id = item.getItemId();
 
-        if(id == R.id.action_settings){
-            return true;
+        if(id == R.id.logout) {
+
+            weDeploy
+                    .auth("https://auth-weread.wedeploy.io")
+                    .signOut()
+                    .execute(new Callback() {
+                        public void onSuccess(Response response) {
+                            Log.d(SemFeed.class.getName(), "saiu");
+                        }
+
+                        public void onFailure(Exception e) {
+                            Log.e(SemFeed.class.getName(), e.getMessage());
+                        }
+                    });
         }
 
         return super.onOptionsItemSelected(item);
