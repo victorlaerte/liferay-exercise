bb5fba2c36978fa1bebfe3f9bde0b5fe8925851c
camisLuisa <clfl@ecomp.poli.br>
2017-09-28 15:05:04 -0300

    testing pr

--- MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/NewUserActivity.java
+++ MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/NewUserActivity.java
@@ -17,7 +17,7 @@ public class NewUserActivity extends AppCompatActivity {
     WeDeploy weDeploy = new WeDeploy.Builder().build();
 //Teste
     //test
-    //test 3
+    //test 34
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
