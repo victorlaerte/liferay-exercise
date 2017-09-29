063db0bf82f796cf4ddecab5f4a6d8582d05be7b
camisLuisa <clfl@ecomp.poli.br>
2017-09-29 10:50:27 -0300

    addNewUrl putExtra Feed

--- MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/NewUrlActivity.java
+++ MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/NewUrlActivity.java
@@ -45,8 +45,9 @@ public class NewUrlActivity extends AppCompatActivity {
         TextView url1 = (TextView) findViewById(R.id.url);
         String url = url1.getText().toString();
         feed = new Feed(nomeUrl,url,userId);
-        intent.putExtra("nomeUrl",nomeUrl);
-        intent.putExtra("url",url);
+        intent.putExtra("feed",feed);
+        //intent.putExtra("nomeUrl",nomeUrl);
+        //intent.putExtra("url",url);
         //intent.putExtra("userId",userId);
         setResult(1,intent);
         finish();
