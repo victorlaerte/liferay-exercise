f1deb63a2da1c3a6c360e781eda3b5d0e7f63c2d
camisLuisa <clfl@ecomp.poli.br>
2017-11-28 14:49:22 -0300

    refactored NewUserActivity

--- MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/activities/NewUserActivity.java
+++ MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/activities/NewUserActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
 import com.example.luisafarias.myapplication.R;
 import com.example.luisafarias.myapplication.model.User;
 import com.example.luisafarias.myapplication.model.WeDeployActions;
@@ -19,136 +20,140 @@ import com.wedeploy.android.auth.Authorization;
 import com.wedeploy.android.auth.TokenAuthorization;
 import com.wedeploy.android.exception.WeDeployException;
 import com.wedeploy.android.transport.Response;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class NewUserActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_new_user);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user);
 
-		User userViewModel = ViewModelProviders.of(this).get(User.class);
-		_editTextName = findViewById(R.id.box_name);
-		_editTextEmail = findViewById(R.id.box_email);
-		_editTextPassword = findViewById(R.id.box_password);
-		_constraintLayout = findViewById(R.id.layout_new_user);
+        User userViewModel = ViewModelProviders.of(this).get(User.class);
+        _editTextName = findViewById(R.id.box_name);
+        _editTextEmail = findViewById(R.id.box_email);
+        _editTextPassword = findViewById(R.id.box_password);
+        _constraintLayout = findViewById(R.id.layout_new_user);
 
-		if (userViewModel.getName() != null
-			|| userViewModel.getEmail() != null
-			|| userViewModel.getPassWord() != null) {
+        if (userViewModel.getName() != null
+                || userViewModel.getEmail() != null
+                || userViewModel.getPassWord() != null) {
 
-			_editTextName.setText(userViewModel.getName());
-			_editTextEmail.setText(userViewModel.getEmail());
-			_editTextPassword.setText(userViewModel.getPassWord());
-		}
-		else {
-			userViewModel.setName(_editTextName.getText().toString());
-			userViewModel.setEmail(_editTextEmail.getText().toString());
-			userViewModel.setPassWord(_editTextPassword.getText().toString());
-		}
-	}
+            _editTextName.setText(userViewModel.getName());
+            _editTextEmail.setText(userViewModel.getEmail());
+            _editTextPassword.setText(userViewModel.getPassWord());
+        } else {
+            userViewModel.setName(_editTextName.getText().toString());
+            userViewModel.setEmail(_editTextEmail.getText().toString());
+            userViewModel.setPassWord(_editTextPassword.getText().toString());
+        }
+    }
 
-	public void createUser(final View view) throws WeDeployException {
-		final String name = _editTextName.getText().toString();
-		final String email = _editTextEmail.getText().toString();
-		final String password = _editTextPassword.getText().toString();
+    public void createUser(final View view) throws WeDeployException {
+        final String name = _editTextName.getText().toString();
+        final String email = _editTextEmail.getText().toString();
+        final String password = _editTextPassword.getText().toString();
 
-		WeDeployActions.getInstance()
-			.createNewUser(email, password, name, new Callback() {
-				@Override
-				public void onSuccess(Response response) {
-					Snackbar.make(_constraintLayout,
-						"Usuario criado com sucesso", Snackbar.LENGTH_LONG)
-						.show();
-					login(email, password);
-				}
+        createNewUser(name, email, password);
+    }
 
-				@Override
-				public void onFailure(Exception e) {
-					Log.e("NewUserActivity", e.getMessage());
-					Snackbar.make(_constraintLayout,
-						"Nao foi possivel criar usuario", Snackbar.LENGTH_LONG)
-						.show();
-				}
-			});
-	}
+    private void createNewUser(String name, final String email, final String password) {
+        WeDeployActions.getInstance()
+                .createNewUser(email, password, name, new Callback() {
+                    @Override
+                    public void onSuccess(Response response) {
+                        Snackbar.make(_constraintLayout,
+                                "Usuario criado com sucesso", Snackbar.LENGTH_LONG)
+                                .show();
+                        login(email, password);
+                    }
 
-	private void login(String email, String password) {
-		WeDeployActions.getInstance().login(email, password, new Callback() {
-			@Override
-			public void onSuccess(Response response) {
-				try {
-					JSONObject jsonObject = new JSONObject(response.getBody());
-					final String token =
-						jsonObject.getString(Constants.ACCESS_TOKEN);
-					currentUser(token);
-				}
-				catch (JSONException e) {
-					Log.e("NewUserActivity", e.getMessage());
-					Snackbar.make(_constraintLayout,
-						"Nao foi possivel fazer login", Snackbar.LENGTH_LONG)
-						.show();
-				}
-			}
+                    @Override
+                    public void onFailure(Exception e) {
+                        Log.e("NewUserActivity", e.getMessage());
+                        Snackbar.make(_constraintLayout,
+                                "Nao foi possivel criar usuario", Snackbar.LENGTH_LONG)
+                                .show();
+                    }
+                });
+    }
 
-			@Override
-			public void onFailure(Exception e) {
-				//TODO: Falta tratamento de erro
-			}
-		});
-	}
+    private void login(String email, String password) {
+        WeDeployActions.getInstance().login(email, password, new Callback() {
+            @Override
+            public void onSuccess(Response response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response.getBody());
+                    final String token =
+                            jsonObject.getString(Constants.ACCESS_TOKEN);
+                    currentUser(token);
+                } catch (JSONException e) {
+                    Log.e("NewUserActivity", e.getMessage());
+                    Snackbar.make(_constraintLayout,
+                            "Nao foi possivel fazer login", Snackbar.LENGTH_LONG)
+                            .show();
+                }
+            }
 
-	public void currentUser(final String token) {
-		Authorization authorization = new TokenAuthorization(token);
-		WeDeployActions.getInstance()
-			.getCurrentUser(authorization, new Callback() {
-				@Override
-				public void onSuccess(Response response) {
-					try {
-						JSONObject jsonObject1 =
-							new JSONObject(response.getBody());
-						String userId = jsonObject1.getString(Constants.ID);
-						saveUser(token, userId);
-						openMainActivity(token, userId);
-					}
-					catch (JSONException e) {
-						e.printStackTrace();
-					}
-				}
+            @Override
+            public void onFailure(Exception e) {
+                Snackbar.make(_constraintLayout,
+                        "Nao foi possivel fazer login", Snackbar.LENGTH_LONG)
+                        .show();
+            }
+        });
+    }
 
-				@Override
-				public void onFailure(Exception e) {
-					Log.e("NewUserActivity", e.getMessage());
-					Snackbar.make(_constraintLayout,
-						"Nao foi possivel acessar usuario",
-						Snackbar.LENGTH_LONG).show();
-				}
-			});
-	}
+    private void currentUser(final String token) {
+        Authorization authorization = new TokenAuthorization(token);
+        WeDeployActions.getInstance()
+                .getCurrentUser(authorization, new Callback() {
+                    @Override
+                    public void onSuccess(Response response) {
+                        try {
+                            JSONObject jsonObject1 =
+                                    new JSONObject(response.getBody());
+                            String userId = jsonObject1.getString(Constants.ID);
+                            saveUser(token, userId);
+                            openMainActivity(token, userId);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
 
-	public void saveUser(String token, String userId) {
-		SharedPreferences sharedP =
-			getSharedPreferences(Constants.USER, MODE_PRIVATE);
-		SharedPreferences.Editor editor = sharedP.edit();
-		editor.putString(Constants.TOKEN, token);
-		editor.putString(Constants.USER_ID, userId);
-		editor.apply();
-	}
+                    @Override
+                    public void onFailure(Exception e) {
+                        Log.e("NewUserActivity", e.getMessage());
+                        Snackbar.make(_constraintLayout,
+                                "Nao foi possivel acessar usuario",
+                                Snackbar.LENGTH_LONG).show();
+                    }
+                });
+    }
 
-	public void openMainActivity(String token, String userId) {
-		Intent intent = new Intent(this, MainActivity.class);
-		Bundle extra = new Bundle();
-		extra.putString(Constants.TOKEN_KEY, token);
-		extra.putString(Constants.USER_ID, userId);
-		intent.putExtra(Constants.TOKEN_USER_ID, extra);
-		finish();
-		startActivity(intent);
-	}
+    private void saveUser(String token, String userId) {
+        SharedPreferences sharedP =
+                getSharedPreferences(Constants.USER, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedP.edit();
+        editor.putString(Constants.TOKEN, token);
+        editor.putString(Constants.USER_ID, userId);
+        editor.apply();
+    }
 
-	private EditText _editTextName;
-	private EditText _editTextEmail;
-	private EditText _editTextPassword;
-	private ConstraintLayout _constraintLayout;
+    private void openMainActivity(String token, String userId) {
+        Intent intent = new Intent(this, MainActivity.class);
+        Bundle extra = new Bundle();
+        extra.putString(Constants.TOKEN_KEY, token);
+        extra.putString(Constants.USER_ID, userId);
+        intent.putExtra(Constants.TOKEN_USER_ID, extra);
+        finish();
+        startActivity(intent);
+    }
+
+    private EditText _editTextName;
+    private EditText _editTextEmail;
+    private EditText _editTextPassword;
+    private ConstraintLayout _constraintLayout;
 }
