808fc69acf7242269c57bb82601b821e7b1d4355
camisLuisa <clfl@ecomp.poli.br>
2017-11-28 18:02:31 -0300

    refactored Rss

--- MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/model/Rss.java
+++ MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/model/Rss.java
@@ -2,6 +2,7 @@ package com.example.luisafarias.myapplication.model;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+
 import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
@@ -12,117 +13,119 @@ import org.simpleframework.xml.Root;
 @Root(name = "rss", strict = false)
 public class Rss implements Parcelable {
 
-	public Rss() {
-	} //para desserializar precisa do construtor vazio public
+    public Rss() {
+    } //para desserializar precisa do construtor vazio public
 
-	public Rss(String url, String userId, Channel channel) {
-		setUrl(url);
-		setUserId(userId);
-		setChannel(channel);
-	}
+    public Rss(String url, String userId, Channel channel) {
+        setUrl(url);
+        setUserId(userId);
+        setChannel(channel);
+    }
 
-	protected Rss(Parcel in) {
-		_url = in.readString();
-		_userId = in.readString();
-		_id = in.readString();
-	}
+    protected Rss(Parcel in) {
+        _url = in.readString();
+        _userId = in.readString();
+        _id = in.readString();
+    }
 
-	public static final Creator<Rss> CREATOR = new Creator<Rss>() {
-		@Override
-		public Rss createFromParcel(Parcel in) {
-			return new Rss(in);
-		}
+    public static final Creator<Rss> CREATOR = new Creator<Rss>() {
+        @Override
+        public Rss createFromParcel(Parcel in) {
+            return new Rss(in);
+        }
 
-		@Override
-		public Rss[] newArray(int size) {
-			return new Rss[size];
-		}
-	};
+        @Override
+        public Rss[] newArray(int size) {
+            return new Rss[size];
+        }
+    };
 
-	public String getUrl() {
-		return this._url;
-	}
+    public String getUrl() {
+        return this._url;
+    }
 
-	public String getUserId() {
-		return this._userId;
-	}
+    public String getUserId() {
+        return this._userId;
+    }
 
-	public String getId() {
-		return this._id;
-	}
+    public String getId() {
+        return this._id;
+    }
 
-	public Channel getChannel() {
-		return _channel;
-	}
+    public Channel getChannel() {
+        return _channel;
+    }
 
-	public String getVersion() {
-		return _version;
-	}
+    public String getVersion() {
+        return _version;
+    }
 
-	public void setChannel(Channel channel) {
-		this._channel = channel;
-	}
+    public void setChannel(Channel channel) {
+        this._channel = channel;
+    }
 
-	public void setUrl(String _url) {
-		this._url = _url;
-	}
+    public void setUrl(String _url) {
+        this._url = _url;
+    }
 
-	public void setId(String _id) {
-		this._id = _id;
-	}
+    public void setId(String _id) {
+        this._id = _id;
+    }
 
-	private void setUserId(String _userId) {
-		this._userId = _userId;
-	}
+    private void setUserId(String _userId) {
+        this._userId = _userId;
+    }
 
-	public String getURLHost() {
-		int a = 0;
-		if (_url.contains(".br")) {
-			a = _url.indexOf(".br/");
-		} else if (_url.contains(".com/")) {
-			a = _url.indexOf(".com/");
-		}
-		return _url.substring(0, (a + 5));
-	}
+    public String getURLHost() {
+        int a = 0;
+        if (_url.contains(".br")) {
+            a = _url.indexOf(".br/");
+        } else if (_url.contains(".com/")) {
+            a = _url.indexOf(".com/");
+        }
+        return _url.substring(0, (a + 5));
+    }
 
-	public String getURLEndPoint() {
-		int a = 0;
-		if (_url.contains(".br")) {
-			a = _url.indexOf(".br/");
-		} else if (_url.contains(".com/")) {
-			a = _url.indexOf(".com/");
-		}
-		return _url.substring((a + 5));
-	}
+    public String getURLEndPoint() {
+        int a = 0;
+        if (_url.contains(".br")) {
+            a = _url.indexOf(".br/");
+        } else if (_url.contains(".com/")) {
+            a = _url.indexOf(".com/");
+        }
+        return _url.substring((a + 5));
+    }
 
-	@Override
-	public int describeContents() {
-		return 0;
-	}
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
-	@Override
-	public void writeToParcel(Parcel parcel, int i) {
-		parcel.writeString(_url);
-		parcel.writeString(_userId);
-		parcel.writeString(_id);
-	}
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(_url);
+        parcel.writeString(_userId);
+        parcel.writeString(_id);
+    }
 
-	@Override
-	public String toString() {
-		return "RSS{"
-			+ "version ='"
-			+ _version
-			+ '\''
-			+ " channel="
-			+ _channel
-			+ '}';
-	}
+    @Override
+    public String toString() {
+        return "RSS{"
+                + "version ='"
+                + _version
+                + '\''
+                + " channel="
+                + _channel
+                + '}';
+    }
 
-	@Attribute(name = "version") private String _version;
-	@Element(name = "channel") private Channel _channel;
-	private String _description;
-	private String _id;
-	private String _image;
-	private String _url;
-	private String _userId;
+    @Attribute(name = "version")
+    private String _version;
+    @Element(name = "channel")
+    private Channel _channel;
+    private String _description;
+    private String _id;
+    private String _image;
+    private String _url;
+    private String _userId;
 }
