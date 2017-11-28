cd2b2116a6d9872cde83c86eb87b38090b588502
camisLuisa <clfl@ecomp.poli.br>
2017-11-28 14:51:13 -0300

    refactored ItemAdapter

--- MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/Adapters/ItemAdapter.java
+++ MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/Adapters/ItemAdapter.java
@@ -10,11 +10,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.example.luisafarias.myapplication.R;
 import com.example.luisafarias.myapplication.activities.NewsActivity;
 import com.example.luisafarias.myapplication.interfaces.ItemClickListener;
 import com.example.luisafarias.myapplication.model.Item;
 import com.example.luisafarias.myapplication.util.Constants;
+
 import java.util.List;
 
 /**
@@ -23,74 +25,74 @@ import java.util.List;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemHolder> {
 
-	public ItemAdapter(Context context, List<Item> feed) {
-		this._feedItems = feed;
-		this._context = context;
-	}
+    public ItemAdapter(Context context, List<Item> feed) {
+        this._feedItems = feed;
+        this._context = context;
+    }
 
-	@Override
-	public ItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-		View view = LayoutInflater.from(parent.getContext())
-			.inflate(R.layout.item_body, parent, false);
+    @Override
+    public ItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_body, parent, false);
 
-		return new ItemHolder(view);
-	}
+        return new ItemHolder(view);
+    }
 
-	@Override
-	public int getItemCount() {
-		return _feedItems.size();
-	}
+    @Override
+    public int getItemCount() {
+        return _feedItems.size();
+    }
 
-	public void updateAnswers(List<Item> items) {
-		this._feedItems = items;
-		notifyDataSetChanged();
-	}
+    public void updateAnswers(List<Item> items) {
+        this._feedItems = items;
+        notifyDataSetChanged();
+    }
 
-	private Item getItem(int adapterPosition) {
-		return _feedItems.get(adapterPosition);
-	}
+    private Item getItem(int adapterPosition) {
+        return _feedItems.get(adapterPosition);
+    }
 
-	@Override
-	public void onAttachedToRecyclerView(RecyclerView recyclerView) {
-		super.onAttachedToRecyclerView(recyclerView);
-	}
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
 
-	@Override
-	public void onBindViewHolder(final ItemHolder holder, int position) {
-		final Item item = _feedItems.get(position);
-		holder.titleTextField.setText(item.getTitle());
+    @Override
+    public void onBindViewHolder(final ItemHolder holder, int position) {
+        final Item item = _feedItems.get(position);
+        holder.titleTextField.setText(item.getTitle());
 
-		holder.view.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(_context, NewsActivity.class);
-				Bundle bundle = new Bundle();
-				bundle.putString(Constants.LINK, item.getLink());
-				intent.putExtra(Constants.LINK, bundle);
-				_context.startActivity(intent);
-			}
-		});
-	}
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(_context, NewsActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString(Constants.LINK, item.getLink());
+                intent.putExtra(Constants.LINK, bundle);
+                _context.startActivity(intent);
+            }
+        });
+    }
 
-	public class ItemHolder extends RecyclerView.ViewHolder {
-		protected TextView titleTextField;
-		protected TextView descriptionTextField;
+    public class ItemHolder extends RecyclerView.ViewHolder {
+        protected TextView titleTextField;
+        protected TextView descriptionTextField;
 
-		private ImageView imageView;
-		private TextView publicationDateTextField;
-		protected View view;
+        private ImageView imageView;
+        private TextView publicationDateTextField;
+        protected View view;
 
-		public ItemHolder(View itemView) {
-			super(itemView);
-			view = itemView;
-			titleTextField = itemView.findViewById(R.id.title);
-			imageView = itemView.findViewById(R.id.image);
-			descriptionTextField = itemView.findViewById(R.id.description);
-			publicationDateTextField = itemView.findViewById(R.id.pubdate);
+        public ItemHolder(View itemView) {
+            super(itemView);
+            view = itemView;
+            titleTextField = itemView.findViewById(R.id.title);
+            imageView = itemView.findViewById(R.id.image);
+            descriptionTextField = itemView.findViewById(R.id.description);
+            publicationDateTextField = itemView.findViewById(R.id.pubdate);
 
-		}
-	}
+        }
+    }
 
-	private List<Item> _feedItems;
-	private Context _context;
+    private List<Item> _feedItems;
+    private Context _context;
 }
