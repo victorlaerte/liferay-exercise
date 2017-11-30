3b9d4c9e95880181881bf51e8c5e7a12d6518852
camisLuisa <clfl@ecomp.poli.br>
2017-11-30 15:15:41 -0300

    CustomApplication realm

---
+++ MyApplication2/app/src/main/java/com/example/luisafarias/myapplication/application/CustomApplication.java
@@ -0,0 +1,26 @@
+package com.example.luisafarias.myapplication.application;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+/**
+ * Created by luisafarias on 30/11/17.
+ */
+
+public class CustomApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .name("realm-rss.realm")
+                .deleteRealmIfMigrationNeeded()
+                .build();
+
+        Realm.setDefaultConfiguration(realmConfiguration);
+
+    }
+}
